505546ff7d5e8645d4fbafd70e166fa4519c6626
Octo-Branch <qkike@icloud.com>
2025-03-10 19:15:39 -0500

    Update recaptcha.js

--- js/recaptcha.js
+++ js/recaptcha.js
@@ -1,20 +1,15 @@
-// Manejo de reCAPTCHA v3
 document.addEventListener('DOMContentLoaded', () => {
-    if(document.querySelector('.contact-form')) {
-        // Generar token inicial
-        grecaptcha.ready(() => {
-            grecaptcha.execute('TU_SITE_KEY', { action: 'contacto' })
-                .then(token => {
-                    document.getElementById('recaptchaResponse').value = token;
-                });
-        });
+    const recaptchaResponse = document.getElementById('recaptchaResponse');
+    
+    if(recaptchaResponse) {
+        const updateRecaptchaToken = () => {
+            grecaptcha.ready(() => {
+                grecaptcha.execute('TU_SITE_KEY', { action: 'contacto' })
+                    .then(token => recaptchaResponse.value = token);
+            });
+        };
 
-        // Renovar token cada 2 minutos
-        setInterval(() => {
-            grecaptcha.execute('TU_SITE_KEY', { action: 'contacto' })
-                .then(token => {
-                    document.getElementById('recaptchaResponse').value = token;
-                });
-        }, 120000);
+        updateRecaptchaToken();
+        setInterval(updateRecaptchaToken, 120000);
     }
 });
